18fcc9c8b7e112a8365548d1fee5d5169ef0d6c8
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-04-08 22:20:05 +0530

    Add files

---
+++ android/app/src/main/kotlin/com/flutterscope/fsepu_shimmer_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutterscope.fsepu_shimmer_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('qr_flutter'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200.0,
+              height: 100.0,
+              child: Shimmer.fromColors(
+                baseColor: Colors.black,
+                highlightColor: Colors.blue,
+                child: const Text(
+                  'Shimmer',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
